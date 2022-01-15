4c9af8fed3d7717e39d513ba8a8dcc0cf318840e
读心悦 <yongyuan253015@gmail.com>
2022-01-15 23:20:38 +0800

    main.ts

---
+++ main.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: yongyuan253015@gmail.com
+ * @Date: 2022-01-14 22:55:44
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-01-14 22:56:56
+ * @Description: 文件描述
+ */
+const arr: number[]= [2,1,3,5]
