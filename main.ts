199017dd430ce538cb7b90430dc495478911180f
读心悦 <yongyuan253015@gmail.com>
2022-01-16 23:26:19 +0800

    ts 应用

--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
-/*
- * @Author: yongyuan253015@gmail.com
- * @Date: 2022-01-14 22:55:44
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-01-15 23:21:44
- * @Description: 文件描述
- */
 const arr: number[]= [2,1,3,5]
 
 const str: Date = new Date()
--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,25 @@
  * @Author: yongyuan253015@gmail.com
  * @Date: 2022-01-09 22:44:27
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-01-09 22:59:33
+ * @LastEditTime: 2022-01-16 17:30:28
  * @Description: 文件描述
  */
 const path = require('path');
-module.exports ={
-    entry: './main.ts',
-    output:{
-        filename: 'text.js',
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
         path: path.join(__dirname, 'dist')
     },
-    mode:'development',
-    module:{
+    mode: 'development',
+    module: {
         rules: [{
-            test: /\.ts?$/,
+            test: /\.tsx?$/,
             use: 'ts-loader',
             exclude: /node_modules/
         }]
     },
     resolve: {
-        extensions: ['.ts']
+        extensions: ['.ts', '.tsx', '.js']
     }
 }
