560498b693c271bbf22f36da6ae914bf1d1f6fff
读心悦 <1638877065@qq.com>
2022-05-10 23:27:59 +0800

    数组

--- src/array.ts
+++ src/array.ts
@@ -1,1 +1,9 @@
-const array: string[] = [1, 2, 4, 5, 6]
+const array: (number | string)[] = [1, "90", 4, 5, 6];
+
+const array1: Array<string | number> = [23, 43, "324"]
+
+const red: ReadonlyArray<number> = [342, 3]
+
+const color: readonly number[] = [32, 546];
+
+const green: Readonly<number[]> = [2, 3]
