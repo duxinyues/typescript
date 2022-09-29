a6871467e1227d9971c6708abe243648cbbd0198
duxinyues <yongyuan253015@gmail.com>
2022-09-29 23:13:37 +0800

    初始化Typescript项目

--- src/numberEnum.ts
+++
@@ -1,8 +0,0 @@
-enum Direction {
-  Up, Down, Left, Right
-}
-
-const direction: number = Direction.Up;
-
-
-console.log(direction)
--- utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.add = void 0;
-function add(x, y) {
-    return x + y;
-}
-exports.add = add;
