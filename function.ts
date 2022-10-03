1357c6b6be9d267ad7ee43274eba7e15816515a5
duxinyues <yongyuan253015@gmail.com>
2022-10-03 16:33:39 +0800

    feat:接口

--- function.ts
+++ function.ts
@@ -2,7 +2,7 @@
  * @Author: duxinyues yongyuan253015@gmail.com
  * @Date: 2022-10-02 20:55:28
  * @LastEditors: duxinyues yongyuan253015@gmail.com
- * @LastEditTime: 2022-10-02 21:36:47
+ * @LastEditTime: 2022-10-03 16:33:21
  * @FilePath: \typescript\function.ts
  * @Description: 
  * Copyright (c) 2022 by duxinyues email: yongyuan253015@gmail.com, All Rights Reserved.
@@ -10,5 +10,7 @@
 function add(num1: number, num2: number = 90) {
     return num1 + num2;
 }
-
-console.log(add(1))//91
+// 接口
+interface Props {
+    new(message?: string): Error;
+}
---
+++ interface.ts
@@ -0,0 +1,37 @@
+/*
+ * @Author: duxinyues yongyuan253015@gmail.com
+ * @Date: 2022-10-03 16:32:33
+ * @LastEditors: duxinyues yongyuan253015@gmail.com
+ * @LastEditTime: 2022-10-03 16:32:34
+ * @FilePath: \typescript\interface.ts
+ * @Description: 
+ * Copyright (c) 2022 by duxinyues email: yongyuan253015@gmail.com, All Rights Reserved.
+ */
+// 接口
+interface Props {
+    new(message?: string): Error;
+}
+
+interface Document {
+    getElementById(element: string): HTMLElement | null
+}
+
+interface A {
+    f(x: boolean): string;
+}
+interface B {
+    f: { (x: boolean): string }
+}
+
+interface C {
+    f: (x: boolean) => string
+}
+
+interface Index{
+    [prop:string]:number;
+    name:0
+}
+
+interface NumberIndex{
+    [prop:number]:string;
+}
