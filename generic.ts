409210d65ec0acdb2e8d410f881dc20dc68807d3
duxinyues <yongyuan253015@gmail.com>
2022-10-05 15:59:39 +0800

    feat:泛型

--- generic.ts
+++ generic.ts
@@ -2,7 +2,7 @@
  * @Author: duxinyues yongyuan253015@gmail.com
  * @Date: 2022-10-05 14:00:34
  * @LastEditors: duxinyues yongyuan253015@gmail.com
- * @LastEditTime: 2022-10-05 15:30:12
+ * @LastEditTime: 2022-10-05 15:52:17
  * @FilePath: \typescript\generic.ts
  * @Description: 泛型
  * Copyright (c) 2022 by duxinyues email: yongyuan253015@gmail.com, All Rights Reserved.
@@ -26,3 +26,21 @@ function move<T extends Point>(arg: T): T {
     return arg;
 }
 move({ x: 90, y: 0, z: 8 })
+
+function f1<T>(x: T): T {
+    return x;
+}
+
+f1<string>('90');
+f1<number>(12);
+
+
+interface MyArray<T> extends Array<T> {
+    first: T | undefined;
+    last: T | undefined;
+}
+
+const ar: Array<number> = [9];
+const ar1: number[] = [0];
+
+type Nullable<T> = T | undefined | null;
