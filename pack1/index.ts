f7f373059b95b4004beb451a22be6972d6bcb75f
duxinyues <yongyuan253015@gmail.com>
2022-10-07 16:37:52 +0800

    feat:多工程引用

---
+++ pack1/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: duxinyues yongyuan253015@gmail.com
+ * @Date: 2022-10-07 16:04:54
+ * @LastEditors: duxinyues yongyuan253015@gmail.com
+ * @LastEditTime: 2022-10-07 16:14:41
+ * @FilePath: \typescript\pack1\index.ts
+ * @Description: 
+ * Copyright (c) 2022 by duxinyues email: yongyuan253015@gmail.com, All Rights Reserved.
+ */
+import { sum } from "../pack/index"
+console.log(sum(90, 2))
